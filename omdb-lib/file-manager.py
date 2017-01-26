43e38adb2a8c9f9c5383f4ee257c0db298d6b525
Soorya Annadurai <soorya.annadurai@gmail.com>
2017-01-27 00:21:28 +0530

    Used ombd-lib functionality in file-manager.py

---
+++ omdb-lib/file-manager.py
@@ -0,0 +1,83 @@
+import os
+import re
+import shutil
+import pathlib
+import Master_Movie_Genre
+
+# myMovieHomeDirectory = "E:\Temp"
+myMovieHomeDirectory = "E:\Movies"
+slashType = "\\" # Use the other slash for Linux-based systems.
+
+
+def createFolderForFile(fileStr):
+    newFilePath = str(fileStr[:(fileStr.rfind("."))])+slashType
+    if(os.path.isdir(newFilePath) == False):
+        os.makedirs(newFilePath)
+    return shutil.move(fileStr, newFilePath)
+
+def extractFolderNameFromDirectoryName(fileStr, isFile):
+    if(isFile):
+        directoryName = str(fileStr[:(fileStr.rfind(slashType))])
+    else:
+        directoryName = fileStr
+    folderName = directoryName[directoryName.rfind(slashType) + 1:]
+    return folderName
+
+def cleanMovieName_leftSide(movieStr):
+    movieStr = movieStr.lstrip()
+    if(movieStr[0] == '['):
+        movieStr = movieStr[movieStr.find(']') + 1:]
+        movieStr = cleanMovieName_leftSide(movieStr)
+    if(movieStr[0] == '('):
+        movieStr = movieStr[movieStr.find(')') + 1:]
+        movieStr = cleanMovieName_leftSide(movieStr)
+    if(movieStr[0] == '{'):
+        movieStr = movieStr[movieStr.find('}') + 1:]
+        movieStr = cleanMovieName_leftSide(movieStr)
+    if(movieStr[0] == '-'):
+        movieStr = movieStr[movieStr.find('-') + 1:]
+        movieStr = cleanMovieName_leftSide(movieStr)
+    return movieStr
+
+def cleanMovieName_rightSide(movieStr):
+    if(movieStr.find('(') != -1):
+        movieStr = movieStr[:movieStr.find('(')].rstrip()
+        movieStr = cleanMovieName_rightSide(movieStr)
+    if(movieStr.find('[') != -1):
+        movieStr = movieStr[:movieStr.find(']')].rstrip()
+        movieStr = cleanMovieName_rightSide(movieStr)
+    if(movieStr.find('{') != -1):
+        movieStr = movieStr[:movieStr.find('}')].rstrip()
+        movieStr = cleanMovieName_rightSide(movieStr)
+    return movieStr
+
+def cleanMovieName(movieStr):
+    return cleanMovieName_rightSide(cleanMovieName_leftSide(movieStr))
+
+def lookForYearFromRawMovieName(movieStr):
+    movieStr = movieStr.replace('(','[')
+    movieStr = movieStr.replace(')',']')
+    movieStr = movieStr.replace('{','[')
+    movieStr = movieStr.replace('}',']')
+    possibilities = (re.findall(r"[^[]*\[([^]]*)\]{0,}", movieStr))
+    myList = []
+    for chance in possibilities:
+        if len(chance) == 4 and chance.isdigit():
+            myList.append(chance)
+    if len(myList) == 1:
+        return myList[0]
+    else:
+        return None
+
+for p in pathlib.Path(myMovieHomeDirectory).iterdir():
+    isFile = False
+    finalPath = str(p)
+    if p.is_file():
+        finalPath = createFolderForFile(str(p))
+        isFile = True
+    rawMovieName = extractFolderNameFromDirectoryName(finalPath, isFile)
+    Master_Movie_Genre.master(cleanMovieName(rawMovieName), lookForYearFromRawMovieName(rawMovieName))
+    # print(rawMovieName)
+    print(cleanMovieName(rawMovieName))
+    # print(lookForYearFromRawMovieName(rawMovieName))
+    # print("\n")
